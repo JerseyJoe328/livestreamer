7bde4e2ae568d425ebc2fc245a0264a1fa1cbdb9
intact <intact.devel@gmail.com>
2016-05-15 13:04:01 +0200

    plugins.mivo_com: Update for API change

--- src/livestreamer/plugins/mivo_com.py
+++ src/livestreamer/plugins/mivo_com.py
@@ -4,18 +4,10 @@ from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
 from livestreamer.stream import HLSStream
 
-ANONYMOUS_USER_ID = "BY-TrgLgK0NMzXb_JB_81DfeOxK_SNH7"
-AUTH_URL = "http://api.mivo.com/v4/web/channels/wms-auth"
 CHANNELS_URL = "http://api.mivo.com/v4/web/channels"
 VIDEO_URL = "http://api.mivo.com/v4/web/videos/{0}"
 
 _url_re = re.compile("http://www.mivo.com/#/(?:live/(?:(?P<channel>[^/?]+))|(?:video/(?P<video_id>\d+)\+\+))")
-
-_auth_schema = validate.Schema(
-    {
-        "sign": validate.text
-    }
-)
 
 _channels_schema = validate.Schema(
     [{
@@ -43,18 +35,16 @@ class MivoCom(Plugin):
 
         channel = match.group("channel")
         if channel is not None:
-            headers = {
-                "Authorization": ANONYMOUS_USER_ID
-            }
-            res = http.get(AUTH_URL, headers=headers)
-            auth = http.json(res, schema=_auth_schema)
-
             res = http.get(CHANNELS_URL)
             channels = http.json(res, schema=_channels_schema)
 
             for c in channels:
                 if c["dataSlug"] == channel:
-                    return HLSStream.parse_variant_playlist(self.session, c["dataUrl"] + auth["sign"])
+                    if "?" in c["dataUrl"]:
+                        playlist_url = c["dataUrl"] + "&video"
+                    else:
+                        playlist_url = c["dataUrl"] + "?video"
+                    return HLSStream.parse_variant_playlist(self.session, playlist_url)
 
         video_id = match.group("video_id")
         if video_id is not None:
